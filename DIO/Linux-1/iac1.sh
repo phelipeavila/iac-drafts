93dec381314d128fb39f44827c0c8352b964f5c7
phelipeavila <phelipe.avila@gmail.com>
2022-11-04 19:44:06 -0300

    rewrite script, making it easier to understand

--- DIO/Linux-1/iac1.sh
+++ DIO/Linux-1/iac1.sh
@@ -2,13 +2,6 @@
 
 echo "Start"
 
-echo "Creating directories ..."
-
-mkdir /publico
-mkdir /adm
-mkdir /ven
-mkdir /sec
-
 echo "Creating groups ..."
 
 groupadd GRP_ADM
@@ -28,6 +21,13 @@ useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Creating directories ..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
 
 echo "Setting security policies ..."
 
